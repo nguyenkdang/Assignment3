4149218d7ec5fd1e7faff2455a2cdefe295e41dc
terryhn1 <57304245+terryhn1@users.noreply.github.com>
2020-02-22 12:01:06 -0800

    Stop Words Bank

---
+++ stop_words.py
@@ -0,0 +1,15 @@
+
+
+#SET OF STOP WORDS TO USE FOR PROJECT 3
+
+IS_STOP_WORD = set(["a", "about","above","after","again","against","all","am","an","and","any","are","aren","as","at",\
+    "be","because","been","before","being","below","between","both","but","by","can","can't","cannot","could","couldn","did",\
+    "didn","do","does","doesn't","doing","don","down","during","each","few","for","from","further","had","hadn","has",\
+    "hasn","have","haven","having","he","he'd","he'll","he's","her","here","here's","hers","herself","him","himself","his",\
+    "how","how's","i","i'd","i'll","i'm","i've","if","in","into","is","isn't","it","it's","its","itself","let's","me","more","most",\
+    "mustn","my","myself","no","nor","not","of","off","on","once","only","or","other","ought","our","ours"	,"ourselves","out",\
+    "over","own","same","shan","she","she'd","she'll","she's","should","shouldn't","so","some","such","than","that","that's","the",\
+    "their","theirs","them","themselves","then","there","there's","these","they","they'd","they'll","they're","they've","this","those",\
+    "through","to","too","under","until","up","very","was","wasn","we","we'd","we'll","we're","we've","were","weren","what","what's",\
+    "when","when's","will","where","where's","which","while","who","who's","whom","why","why's","with","won't","would","wouldn","you","you'd",\
+    "you'll","you're","you've","your","yours","yourself","yourselves"])
